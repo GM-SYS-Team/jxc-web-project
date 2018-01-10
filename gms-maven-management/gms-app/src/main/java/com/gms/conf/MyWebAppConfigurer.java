75d7e601ac76b749c9ba67f24f913e274c0becb4
qian <qian@qian.com>
2018-01-10 17:46:41 +0800

    options请求，返回Ok

--- gms-maven-management/gms-app/src/main/java/com/gms/conf/MyWebAppConfigurer.java
+++ gms-maven-management/gms-app/src/main/java/com/gms/conf/MyWebAppConfigurer.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.gms.interceptor.CORSInterceptor;
 import com.gms.interceptor.UserInterceptor;
 
 @Configuration
@@ -18,12 +19,18 @@ public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
 		return new UserInterceptor();
 	}
 	
+	@Bean
+	public HandlerInterceptor getCORSInterceptor() {
+		return new CORSInterceptor();
+	}
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// 多个拦截器组成一个拦截器链
 		// addPathPatterns 用于添加拦截规则
 		// excludePathPatterns 用户排除拦截
 		registry.addInterceptor(getUserInterceptor()).addPathPatterns("/**");
+		registry.addInterceptor(getCORSInterceptor()).addPathPatterns("/**");
 		super.addInterceptors(registry);
 	}
 
