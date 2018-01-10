e08b6d62c042cd3c77baddf8ea4874eb1eebedd8
qian <qian@qian.com>
2018-01-10 18:12:21 +0800

    跨域处理

--- gms-maven-management/gms-app/src/main/java/com/gms/interceptor/CORSInterceptor.java
+++ gms-maven-management/gms-app/src/main/java/com/gms/interceptor/CORSInterceptor.java
@@ -13,14 +13,15 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 public class CORSInterceptor extends HandlerInterceptorAdapter {
 
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-		if (request.getMethod().equalsIgnoreCase("options")) {
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
+		response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
+		response.setHeader("Access-Control-Allow-Credentials","true");
+		response.setHeader("Access-Control-Allow-Methods","POST, GET, OPTIONS, DELETE, OPTIONS");
+		response.setHeader("Access-Control-Allow-Headers","Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+		
+		if(request.getMethod().equalsIgnoreCase("options")){
 			response.setStatus(200);
-			response.getWriter().println("OK");
+			response.getWriter().println("ok");
 			return false;
 		}
 		return true;
