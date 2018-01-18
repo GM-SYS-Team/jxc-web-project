da9cb567f51aa627ceac36d65200050cfeb54149
qian <qian@qian.com>
2018-01-18 15:23:58 +0800

    添加优惠券逻辑删除功能

--- gms-maven-management/gms-dao/src/main/java/com/gms/dao/repository/CouponRepository.java
+++ gms-maven-management/gms-dao/src/main/java/com/gms/dao/repository/CouponRepository.java
@@ -26,32 +26,32 @@ public interface CouponRepository extends JpaRepository<Coupon, Integer>,JpaSpec
 	@Query(value="delete from t_coupon where id = ?1",nativeQuery=true)
 	public void deleteCoupon(Integer couponId);
 	
-	@Query(value="select * from t_coupon where expiry_date_start>?1 and shop_id = ?2 order by id desc",nativeQuery=true)
+	@Query(value="select * from t_coupon where expiry_date_start>?1 and shop_id = ?2 and status != 4 order by id desc",nativeQuery=true)
 	public List<Coupon> findCouponBygt(Date date,Integer shopId);
 	
-	@Query(value="select * from t_coupon where expiry_date_stop<?1 and shop_id = ?2 order by id desc",nativeQuery=true)
+	@Query(value="select * from t_coupon where expiry_date_stop<?1 and shop_id = ?2 and status != 4 order by id desc",nativeQuery=true)
 	public List<Coupon> findCouponBylt(Date date,Integer shopId);
 	
-	@Query(value="select * from t_coupon where expiry_date_stop>=?1 and expiry_date_start<=?1 and shop_id = ?2 order by id desc",nativeQuery=true)
+	@Query(value="select * from t_coupon where expiry_date_stop>=?1 and expiry_date_start<=?1 and shop_id = ?2 and status != 4 order by id desc",nativeQuery=true)
 	public List<Coupon> findCouponBybt(Date date,Integer shopId);
 	
-	@Query(value="select * from t_coupon where shop_id = ?1",nativeQuery=true)
+	@Query(value="select * from t_coupon  where shop_id = ?1 and status != 4",nativeQuery=true)
 	public List<Coupon> findCouponAll(Integer shopId);
 	
 	
-	@Query(value="select * from t_coupon where expiry_date_start>?1 order by id desc",nativeQuery=true)
+	@Query(value="select * from t_coupon where expiry_date_start>?1 and status != 4 order by id desc",nativeQuery=true)
 	public List<Coupon> findCouponBygtAdmin(Date date);
 	
-	@Query(value="select * from t_coupon where expiry_date_stop<?1 order by id desc",nativeQuery=true)
+	@Query(value="select * from t_coupon where expiry_date_stop<?1 and status != 4 order by id desc",nativeQuery=true)
 	public List<Coupon> findCouponByltAdmin(Date date);
 	
-	@Query(value="select * from t_coupon where expiry_date_stop>=?1 and expiry_date_start<=?1 order by id desc",nativeQuery=true)
+	@Query(value="select * from t_coupon where expiry_date_stop>=?1 and expiry_date_start<=?1 and status != 4 order by id desc",nativeQuery=true)
 	public List<Coupon> findCouponBybtAdmin(Date date);
 	
-	@Query(value="select * from t_coupon",nativeQuery=true)
+	@Query(value="select * from t_coupon where status != 4",nativeQuery=true)
 	public List<Coupon> findCouponAllAdmin();
 
-	@Query(value="select * from t_coupon where expiry_date_stop>?1 and shop_id != ?2 and status = 1 limit 1", nativeQuery=true)
+	@Query(value="select * from t_coupon where expiry_date_stop>?1 and shop_id != ?2 and status = 1 and status != 4 limit 1", nativeQuery=true)
 	public Coupon findRandomCoupon(Date now, Integer id);
 	
 }
